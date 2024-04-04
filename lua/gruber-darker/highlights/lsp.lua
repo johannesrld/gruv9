19478b8ac3064b0d0c5ddfbb59ba308e00081ac9
johannesrld <71744831+johannesrld@users.noreply.github.com>
2024-04-04 23:02:38 +0800

    python syntax highlighting is wack

--- lua/gruber-darker/highlights/lsp.lua
+++ lua/gruber-darker/highlights/lsp.lua
@@ -67,5 +67,6 @@ M.highlights.lsp_type_namespace = Highlight.new("@lsp.type.namespace", { link = 
 M.highlights.lsp_typemod_variable = Highlight.new("@lsp.typemod.variable.readonly", { link = gruber_hl.fg_1 })
 M.highlights.lsp_type_variable = Highlight.new("@lsp.type.variable", { link = gruber_hl.fg1 })
 M.highlights.lsp_type_variable_python = Highlight.new("@lsp.type.variable.python", {})
+M.highlights.lsp_typemod_variable = Highlight.new("@lsp.typemod.variable.readonly.python", {})
 
 return M
